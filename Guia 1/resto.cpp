484a264a9dcc26a4fd9b1f5f272ea313a83c79da
RodrigoAntunez <71909125+RodrigoAntunez@users.noreply.github.com>
2024-05-07 16:29:23 -0300

    Add files via upload

---
+++ Guia 1/resto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int importe, resto;
+    
+    cout << "Monto : " ;
+    cin >> importe;
+
+int billetes1000 = importe / 1000;
+    importe %= 1000;
+
+
+}
